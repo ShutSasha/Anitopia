194d15641d0c9e959933257ac2d73504e462e4e3
ShutSasha <sanyashut159@gmail.com>
2024-08-09 16:24:10 +0300

    feat: add cookie parser, enable cors

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app/app.module'
 import { SwaggerModule } from '@nestjs/swagger'
 import { config, swaggerPath } from './swagger'
+import * as cookieParser from 'cookie-parser'
 
 const PORT = process.env.PORT || 5000
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
+
+  app.setGlobalPrefix('api')
+
+  app.enableCors({
+    credentials: true,
+    origin: true,
+  })
+
+  app.use(cookieParser())
 
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup(swaggerPath, app, document)
